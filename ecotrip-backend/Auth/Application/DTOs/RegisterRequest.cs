9e1db00b96637e9887e9a220ea004656335bd367
abraam16 <u202219199@upc.edu.pe>
2025-06-18 22:14:39 -0500

    feat: fix errors

--- ecotrip-backend/Auth/Application/DTOs/RegisterRequest.cs
+++ ecotrip-backend/Auth/Application/DTOs/RegisterRequest.cs
@@ -1,10 +1,9 @@
-﻿
-namespace ecotrip_backend.Auth.Application.DTOs
+﻿namespace ecotrip_backend.Auth.Application.DTOs
 {
     public class RegisterRequest
     {
         public string Email { get; set; }
         public string Password { get; set; }
-        public string UserType { get; set; } 
-       
+        public string UserType { get; set; }         
+    } 
 }
